814597c5ce4ddfb23e33b5db7da9f9b5a2403d36
janson <janson_j_shan@163.com>
2022-03-16 15:26:27 +0800

    rpc proxy

--- rpc-consumer/src/main/java/org/janson/consumer/controller/RpcInvokerProxy.java
+++ rpc-consumer/src/main/java/org/janson/consumer/controller/RpcInvokerProxy.java
@@ -1,9 +1,21 @@
 package org.janson.consumer.controller;
 
+import io.netty.channel.DefaultEventLoop;
+import io.netty.util.concurrent.DefaultPromise;
+import org.janson.core.RpcFuture;
+import org.janson.core.RpcRequest;
+import org.janson.core.RpcRequestHolder;
+import org.janson.core.RpcResponse;
+import org.janson.protocol.protocol.MsgHeader;
+import org.janson.protocol.protocol.MsgType;
+import org.janson.protocol.protocol.ProtocolConstants;
+import org.janson.protocol.protocol.RpcProtocol;
+import org.janson.protocol.serialization.SerializationTypeEnum;
 import org.janson.registry.RegistryService;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description:
@@ -23,6 +35,30 @@ public class RpcInvokerProxy implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        return null;
+        RpcProtocol<RpcRequest> protocol = new RpcProtocol<>();
+        MsgHeader header = new MsgHeader();
+        long requestId = RpcRequestHolder.REQUEST_ID_GEN.incrementAndGet();
+        header.setMagic(ProtocolConstants.MAGIC);
+        header.setVersion(ProtocolConstants.VERSION);
+        header.setRequestId(requestId);
+        header.setSerialization((byte) SerializationTypeEnum.HESSIAN.getType());
+        header.setMsgType((byte) MsgType.REQUEST.getType());
+        header.setStatus((byte) 0x1);
+        protocol.setHeader(header);
+
+        RpcRequest request = new RpcRequest();
+        request.setServiceVersion(this.serviceVersion);
+        request.setClassName(method.getDeclaringClass().getName());
+        request.setMethodName(method.getName());
+
+        request.setParameterTypes(method.getParameterTypes());
+        request.setParams(args);
+        protocol.setBody(request);
+
+        RpcConsumer rpcConsumer = new RpcConsumer();
+        RpcFuture<RpcResponse> future = new RpcFuture<>(new DefaultPromise<>(new DefaultEventLoop()), timeout);
+        RpcRequestHolder.REQUEST_MAP.put(requestId, future);
+        rpcConsumer.sendRequest(protocol, this.registryService);
+        return future.getPromise().get(future.getTimeout(), TimeUnit.SECONDS).getData();
     }
 }
--- rpc-core/src/main/java/org/janson/core/RpcRequestHolder.java
+++ rpc-core/src/main/java/org/janson/core/RpcRequestHolder.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
  **/
 public class RpcRequestHolder {
 
-    public static final AtomicLong REQUEST_ID = new AtomicLong(0);
+    public static final AtomicLong REQUEST_ID_GEN = new AtomicLong(0);
 
     public static final Map<Long, RpcFuture<RpcResponse>> REQUEST_MAP = new ConcurrentHashMap<>();
 }
